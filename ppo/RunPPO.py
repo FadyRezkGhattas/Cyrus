970f65cb83ef6197ab891a811193d9b19abf4c82
Fady Rezk <fady.rezk@tmentors.com>
2023-06-12 18:39:29 +0100

    Encapsulate PPO task in a class

---
+++ ppo/RunPPO.py
@@ -0,0 +1,93 @@
+import random
+import time
+from distutils.util import strtobool
+from functools import partial
+from typing import Any
+import dataclasses as dc
+
+import envpool
+import flax
+import flax.linen as nn
+import gym
+import jax
+import jax.numpy as jnp
+import numpy as np
+import optax
+import chex
+from learned_optimization.research.general_lopt import prefab
+from flax.training.train_state import TrainState
+from torch.utils.tensorboard import SummaryWriter
+from common import *
+from PPOTask import *
+
+class VeloState(TrainState):
+    # A simple extension of TrainState to also include batch statistics
+    # If a model has no batch statistics, it is None
+    batch_stats : Any = None
+    # You can further extend the TrainState by any additional part here
+    # For example, rng to keep for init, dropout, etc.
+    rng : Any = None
+    # Save loss as a state because learned optimizers need it as input
+    # Strange initialization because mutable jaxlib.xla_extension.ArrayImpl is not allowed (must use default_factory): https://github.com/google/jax/issues/14295
+    loss : Any = dc.field(default_factory=lambda: jnp.asarray(0))
+
+    def apply_gradients(self, *, grads, **kwargs):
+        # Clipping gradients as implemented here: https://github.com/deepmind/optax/blob/master/optax/_src/clipping.py#L91
+        # replicating logic to avoid editing the chain operation to accept extra_args as velo expects
+        g_norm = optax.global_norm(grads)
+        trigger = jnp.squeeze(g_norm < args.max_grad_norm)
+        chex.assert_shape(trigger, ())  # A scalar.
+        def clip_fn(t):
+            return jax.lax.select(trigger, t, (t / g_norm.astype(t.dtype)) * args.max_grad_norm)
+        grads = jax.tree_util.tree_map(clip_fn, grads)
+    
+        # Change update signature to pass loss as expected by VeLO
+        updates, new_opt_state = self.tx.update(grads, self.opt_state, self.params, extra_args={"loss": self.loss})
+        new_params = optax.apply_updates(self.params, updates)
+        
+        return self.replace(
+            step = self.step + 1,
+            params = new_params,
+            opt_state = new_opt_state,
+            **kwargs
+        )
+
+if __name__ == '__main__':
+    args = parse_args()
+    use_velo = 'velo' if args.use_velo else 'adam'
+    run_name = f"{use_velo}__{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    if args.track:
+        import wandb
+
+        wandb.init(
+            project=args.wandb_project_name,
+            entity=args.wandb_entity,
+            sync_tensorboard=True,
+            config=vars(args),
+            name=run_name,
+            monitor_gym=True,
+            save_code=True,
+        )
+    writer = SummaryWriter(f"runs/{run_name}")
+    writer.add_text(
+        "hyperparameters",
+        "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    )
+
+    # TRY NOT TO MODIFY: seeding
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    key = jax.random.PRNGKey(args.seed)
+
+    ppo_task = PPOTask(args)
+    key = ppo_task.init(key)
+
+    total_steps = args.num_updates * args.update_epochs * args.num_minibatches
+    agent_state = VeloState.create(
+        apply_fn=None,
+        params=ppo_task.params,
+        tx=prefab.optax_lopt(total_steps)
+    )
+
+    for i in range(1000):
+        agent_state, key = ppo_task.update(agent_state, key)
