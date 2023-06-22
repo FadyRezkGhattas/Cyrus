7a9db9f61f18424949de5a5535ef1ad6df49eb9d
Fady Rezk <fady.rezk@tmentors.com>
2023-06-22 15:45:16 +0100

    add classification trainer

--- convex_optimizaiton/LinearTrainer.py
+++ convex_optimizaiton/LinearTrainer.py
@@ -48,3 +48,35 @@ class LinearTrainer(VeloTrainerModule):
     def print_tabulate(self, exmp_input):
         x, y = exmp_input
         print(self.model.tabulate(rngs={'params': jax.random.PRNGKey(0)}, x=x))
+
+class LinearClassifierTrainer(LinearTrainer):
+    def __init__(self, model_class,
+                 **kwargs):
+        super().__init__(model_class=model_class,
+                         **kwargs)
+        
+    def create_functions(self):
+        def loss_function(params, batch):
+            x, labels = batch
+            logits = self.model.apply({'params': params}, x)
+            loss = optax.softmax_cross_entropy_with_integer_labels(logits, labels).mean()
+            acc = (logits.argmax(axis=-1) == labels).mean()
+            return loss, acc
+        
+        def train_step(state, batch):
+            loss_fn = lambda params : loss_function(params, batch)
+            ret, grads = jax.value_and_grad(loss_fn, has_aux=True)(state.params)
+            loss, acc = ret[0], ret[1]
+            state, updates = state.apply_gradients(grads=grads, loss=loss, return_updates=True)
+            grads, updates = grads.unfreeze(), updates.unfreeze()
+            grads, updates = jax.tree_util.tree_leaves(grads), jax.tree_util.tree_leaves(updates)
+            grads = jnp.concatenate([jnp.ravel(grad) for grad in grads])
+            updates = jnp.concatenate([jnp.ravel(update) for update in updates])
+            update_direction = jnp.dot(grads, updates)
+            metrics = {'loss': loss, 'accuracy': acc, 'update_direction': update_direction}
+            return state, metrics
+        
+        def eval_step(state, batch):
+            return {}
+
+        return train_step, eval_step
