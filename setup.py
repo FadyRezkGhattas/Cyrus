826532f9eedafb39034ebdd458b6589aee592818
Fady Rezk <fady.rezk@tmentors.com>
2023-08-03 09:20:23 +0300

    convert VeLO to a python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""Python setup.py for project_name package"""
+from setuptools import setup
+
+setup(
+    name="project_name",
+    version="0.1",
+    description="project_description",
+    url="https://github.com/FadyRezkGhattas/Cyrus",
+    author="Fady Rezk",
+    py_modules=[]
+)
